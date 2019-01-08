5967b5d86adb6a2080e000c3e3d531387e872802
Moustick2590 <aurel.caillaud@gmail.com>
2019-01-08 16:17:15 +0100

    animations

--- js/main.js
+++ js/main.js
@@ -4,62 +4,57 @@ let cowboyX = "";
 const diametreCowboy = parseFloat(getComputedStyle(cowboy).width); // Conversion en nombre du diametre du ballon
 const xMin = 0; // Position gauche minimale
 const xMaxCowboy = parseFloat(getComputedStyle(cadreCowboy).width);
-const ennemis = document.getElementById('ennemis');
-const ennemi1 = document.getElementById('ennemi_1');
+const jouerBtn = document.getElementById("jouer");
+const cadreEnnemis = document.getElementById("cadre_ennemis");
 
-/*var cadre = document.getElementById("cadre");
-var bandit = document.getElementById("bandit");
-var vitesse = 3; // Valeur du déplacement en pixels
-var diametreBandit = parseFloat(getComputedStyle(bandit).width); // Conversion en nombre du diametre du bandit
-var animationId = null; // Identifiant de l'animation
-var xMin = 0; // Position gauche minimale
-var direction = 1; // Sens de déplacement: 1 droite, 2 gauche
+const bandit = document.getElementById("bandit");
+const vitesse = 8; // Valeur du déplacement en pixels
+const diametreBandit = parseFloat(getComputedStyle(bandit).width); // Conversion en nombre du diametre du bandit
+let animationId = null; // Identifiant de l'animation
+let direction = 1; // Sens de déplacement: 1 droite, 2 gauche
 
-// D2place le ballon vers la gauche ou la droite
-function animerEnnemis() {
+const presentation = document.getElementById('presentation');
+let newYBandit = "";
+cadreEnnemis.removeChild(bandit);
+
+// D2place le bandit vers la gauche ou la droite
+function animerBandit() {
     // Conversion en nombre de la position gauche du bloc
-    var xBallon = parseFloat(getComputedStyle(ballon).left);
-    var yBallon = parseFloat(getComputedStyle(ballon).bottom);
-    var newYBallon = "";
-    // D2place le bandit vers la gauche ou la droite
-    function animerBandit() {
-        // Conversion en nombre de la position gauche du bloc
-        var xBandit = parseFloat(getComputedStyle(bandit).left);
-        // Conversion en nombre de la largeur du cadre
-        var xMax = parseFloat(getComputedStyle(cadre).width);
-        // Si le ballon arrive a un bord du cadre
-        if ((xBandit + diametreBandit > xMax) || (xBandit < xMin)) {
-            // On inverse le sens de déplacement
-            direction *= -1;
-            var newYBallon = yBallon - 10;
-        }
-        ballon.style.bottom = newYBallon + "px";
-        // Déplacement du ballon dans le sens actuel
-        ballon.style.left = (xBallon + vitesse * direction) + "px";
-        // Demande au navigateur d'appeler animerBallon dès que possible
-        animationId = requestAnimationFrame(animerEnnemis);
-        // Déplacement du bandit dans le sens actuel
-        bandit.style.left = (xBandit + vitesse * direction) + "px";
-        // Demande au navigateur d'appeler animerBandit dès que possible
-        animationId = requestAnimationFrame(animerBandit);
+    let xBandit = parseFloat(getComputedStyle(bandit).left);
+    // Position basse du bloc
+    let yBandit = parseFloat(getComputedStyle(bandit).bottom);
+
+    const xMaxBandit = parseFloat(getComputedStyle(cadreEnnemis).width);
+    // Si le ballon arrive a un bord du cadre
+    if ((xBandit + diametreBandit > xMaxBandit) || (xBandit < xMin)) {
+        // On inverse le sens de déplacement
+        direction *= -1;
+        newYBandit = yBandit - 50;
     }
-};
+    // On descend le cadre
+    bandit.style.bottom = newYBandit + "px";
+    // Déplacement du bandit dans le sens actuel
+    bandit.style.left = (xBandit + vitesse * direction) + "px";
+    // Demande au navigateur d'appeler animerBandit dès que possible
+    animationId = requestAnimationFrame(animerBandit);
 
-var jouerBtn = document.getElementById("jouer");
-//var arreterBtn = document.getElementById("arreter");
+}
 
-demarrerBtn.addEventListener("click", function () {
+
+jouerBtn.addEventListener("click", function() {
     // Change l'état des boutons
-    jouerBtn.disabled = true;
+    cadreEnnemis.removeChild(presentation);
+    cadreEnnemis.appendChild(bandit);
     //arreterBtn.disabled = false;
     // Démarre l'animation
     requestAnimationFrame(animerBandit);
-});
+})
 
-arreterBtn.addEventListener("click", function () {
+
+/*arreterBtn.addEventListener("click", function() {
     demarrerBtn.disabled = false;
     arreterBtn.disabled = true;
-    arreterBtn.addEventListener("click", function () {
+    arreterBtn.addEventListener("click", function() {
         jouerBtn.disabled = false;
         //arreterBtn.disabled = true;
         cancelAnimationFrame(animationId);
