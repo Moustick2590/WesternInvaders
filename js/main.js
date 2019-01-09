dedcdd0897be2cafafaaac000bf788dfb5f77d6c
Moustick2590 <aurel.callaud@gmail.com>
2019-01-09 06:12:30 +0100

    petites modif

--- js/main.js
+++ js/main.js
@@ -1,26 +1,22 @@
 /* fonction jouer */
-var jouer = document.getElementById("boutonjouer");
-var explication = document.getElementById("explication");
-
-
-
-
+const jouer = document.getElementById("boutonjouer");
+const explication = document.getElementById("explication");
 const cadreCowboy = document.getElementById("cadre_cowboy");
 const cowboy = document.getElementById("cowboy");
 let cowboyX = "";
+const topCowboy = (parseFloat(getComputedStyle(cowboy).bottom)) + (parseFloat(getComputedStyle(cowboy).height));
 const diametreCowboy = parseFloat(getComputedStyle(cowboy).width); // Conversion en nombre du diametre du ballon
 const xMin = 0; // Position gauche minimale
 const xMaxCowboy = parseFloat(getComputedStyle(cadreCowboy).width);
-
 const cadreEnnemis = document.getElementById("cadre_ennemis");
-
-
+const xMaxBandit = parseFloat(getComputedStyle(cadreEnnemis).width);
 const bandit = document.getElementById("bandit");
-const vitesse = 8; // Valeur du déplacement en pixels
+let xBandit = "";
+const vitesse = 5; // Valeur du déplacement en pixels
 const diametreBandit = parseFloat(getComputedStyle(bandit).width); // Conversion en nombre du diametre du bandit
 let animationId = null; // Identifiant de l'animation
 let direction = 1; // Sens de déplacement: 1 droite, 2 gauche
-
+let yBandit = "";
 const presentation = document.getElementById('presentation');
 let newYBandit = "";
 cadreEnnemis.removeChild(bandit);
@@ -28,13 +24,13 @@ cadreEnnemis.removeChild(bandit);
 // D2place le bandit vers la gauche ou la droite
 function animerBandit() {
     // Conversion en nombre de la position gauche du bloc
-    let xBandit = parseFloat(getComputedStyle(bandit).left);
+    xBandit = parseFloat(getComputedStyle(bandit).left);
     // Position basse du bloc
-    let yBandit = parseFloat(getComputedStyle(bandit).bottom);
+    yBandit = parseFloat(getComputedStyle(bandit).bottom);
 
-    const xMaxBandit = parseFloat(getComputedStyle(cadreEnnemis).width);
+    
     // Si le ballon arrive a un bord du cadre
-    if ((xBandit + diametreBandit > xMaxBandit) || (xBandit < xMin)) {
+    if (((xBandit + diametreBandit) >= xMaxBandit) || (xBandit < xMin)) {
         // On inverse le sens de déplacement
         direction *= -1;
         newYBandit = yBandit - 50;
@@ -47,14 +43,19 @@ function animerBandit() {
     animationId = requestAnimationFrame(animerBandit);
 
 }
+
+// Ajout de la fonction sur le bouton jouer
 jouer.addEventListener("click", function() {
-    cadreEnnemis.removeChild(jouer);
-    cadreEnnemis.removeChild(explication);
-    cadreEnnemis.appendChild(bandit);
-    requestAnimationFrame(animerBandit);
+    cadreEnnemis.removeChild(jouer); // On enlève le bouton jouer
+    cadreEnnemis.removeChild(explication); // On enlève le texte
+    cadreEnnemis.appendChild(bandit); // On ajoute les bandits
+    requestAnimationFrame(animerBandit); // On démarre l'animation des bandits
 })
 
+if (yBandit <= topCowboy) {
+    cancelAnimationFrame(animerBandit);
 
+}
 /*arreterBtn.addEventListener("click", function() {
     demarrerBtn.disabled = false;
     arreterBtn.disabled = true;
@@ -93,3 +94,13 @@ function droite() {
     };
     cowboy.style.left = cowboyX + "px";
 }
+
+// Collision des bandits avec le cowboy
+
+/*if (rect1.x < rect2.x + rect2.width &&
+   rect1.x + rect1.width > rect2.x &&
+   rect1.y < rect2.y + rect2.height &&
+   rect1.height + rect1.y > rect2.y) {
+    // collision détectée !
+}*/
+
