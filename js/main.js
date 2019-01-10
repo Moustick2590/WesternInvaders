a6dbddc406d91ed422c2cd4ceb0b95658beeb8a7
Moustick2590 <aurel.caillaud@gmail.com>
2019-01-10 13:18:51 +0100

    ennemis disparaissent au niveau du cowboy

--- js/main.js
+++ js/main.js
@@ -3,24 +3,24 @@ const jouer = document.getElementById("boutonjouer");
 const explication = document.getElementById("explication");
 const cadreCowboy = document.getElementById("cadre_cowboy");
 const cowboy = document.getElementById("cowboy");
-let cowboyX = "";
+let cowboyX = parseFloat(getComputedStyle(cowboy).left);
 const topCowboy = (parseFloat(getComputedStyle(cowboy).bottom)) + (parseFloat(getComputedStyle(cowboy).height));
 const diametreCowboy = parseFloat(getComputedStyle(cowboy).width); // Conversion en nombre du diametre du ballon
 const xMin = 0; // Position gauche minimale
 const xMaxCowboy = parseFloat(getComputedStyle(cadreCowboy).width);
-const cadreEnnemis = document.getElementById("cadre_ennemis");
-const xMaxBandit = parseFloat(getComputedStyle(cadreEnnemis).width);
-const bandit = document.getElementById("bandit");
-let xBandit = "";
-const vitesseBandit = 5; // Valeur du déplacement en pixels
-const diametreBandit = parseFloat(getComputedStyle(bandit).width); // Conversion en nombre du diametre du bandit
+const cadreJeu = document.getElementById("cadre_jeu");
+const xMaxBandits = parseFloat(getComputedStyle(cadreJeu).width);
+const bandits = document.getElementById("bandits");
+let xBandits = "";
+const vitesseBandits = 3; // Valeur du déplacement en pixels
+const diametreBandits = parseFloat(getComputedStyle(bandits).width); // Conversion en nombre du diametre du bandit
 let animationId = null; // Identifiant de l'animation
 let direction = 1; // Sens de déplacement: 1 droite, 2 gauche
-let yBandit = "";
-let newYBandit = "";
-cadreEnnemis.removeChild(bandit);
+let yBandits = "";
+let newYBandits = "";
+cadreJeu.removeChild(bandits);
 const wantedList = document.getElementById("wantedlist");
-const vitesseBullet = 7;
+const vitesseBullet = 4;
 
 // defilement du texte
 function machineEcrire() {
@@ -34,47 +34,52 @@ message = "Bienvenue au Far West ! Le shérif est mort, vous êtes notre unique 
 cour = 0;
 animation = setInterval("machineEcrire()", 50);
 
+
+
 // D2place le bandit vers la gauche ou la droite
-function animerBandit() {
+function animerBandits() {
     // Conversion en nombre de la position gauche du bloc
-    xBandit = parseFloat(getComputedStyle(bandit).left);
+    xBandits = parseFloat(getComputedStyle(bandits).left);
     // Position basse du bloc
-    yBandit = parseFloat(getComputedStyle(bandit).bottom);
+    yBandits = parseFloat(getComputedStyle(bandits).bottom);
 
 
     // Si le ballon arrive a un bord du cadre
-    if (((xBandit + diametreBandit) >= xMaxBandit) || (xBandit < xMin)) {
+    if (((xBandits + diametreBandits) >= xMaxBandits) || (xBandits < xMin)) {
         // On inverse le sens de déplacement
         direction *= -1;
-        newYBandit = yBandit - 50;
+        newYBandits = yBandits - 30;
     }
     // On descend le cadre
-    bandit.style.bottom = newYBandit + "px";
+    bandits.style.bottom = newYBandits + "px";
     // Déplacement du bandit dans le sens actuel
-    bandit.style.left = (xBandit + vitesseBandit * direction) + "px";
+    bandits.style.left = (xBandits + vitesseBandits * direction) + "px";
     // Demande au navigateur d'appeler animerBandit dès que possible
-    animationId = requestAnimationFrame(animerBandit);
-
+    animationId = requestAnimationFrame(animerBandits);
+    // Collision des bandits avec le cowboy
+    if (yBandits <= topCowboy) {
+        cadreJeu.removeChild(bandits);
+        gameOver();
+    }
 }
 // fonction changement bg
 function changeBackground(bElement, bUrl) {
     return bElement.style.backgroundImage = "url(" + bUrl + ")", "cover", "no-repeat", "center";
 }
 
+
 // Ajout de la fonction sur le bouton jouer
 jouer.addEventListener("click", function() {
-    cadreEnnemis.removeChild(jouer); // On enlève le bouton jouer
-    cadreEnnemis.removeChild(explication); // On enlève le texte
-    cadreEnnemis.appendChild(bandit); // On ajoute les bandits
-    cadreEnnemis.removeChild(wantedList); // On enleve les affiches wanted
-    requestAnimationFrame(animerBandit); // On démarre l'animation des bandits
+    jouer.style.display = "none"; // On enlève le bouton jouer
+    explication.style.display = "none"; // On enlève le texte
+    /*genBandit();*/
+    cadreJeu.appendChild(bandits); // On ajoute les bandits
+    wantedList.style.display = "none"; // On enleve les affiches wanted
+    requestAnimationFrame(animerBandits); // On démarre l'animation des bandits
     changeBackground(document.body, "images/bg_scene_1.jpg");
 })
 
-if (yBandit <= topCowboy) {
-    cancelAnimationFrame(animerBandit);
 
-}
 /*arreterBtn.addEventListener("click", function() {
     demarrerBtn.disabled = false;
     arreterBtn.disabled = true;
@@ -90,33 +95,33 @@ if (yBandit <= topCowboy) {
 document.onkeydown = function(event) {
     if (event.keyCode == 37) gauche();
     if (event.keyCode == 39) droite();
-    if (event.keyCode == 38) tir();
+    if ((event.keyCode == 38) || (event.keyCode == 32)) tir();
 }
 
 // Variables déplacement
 
 // A GAUCHE
 function gauche() {
-    cowboyX = getComputedStyle(cowboy).left;
-    cowboyX = parseInt(cowboyX) - 10;
+
+    cowboyX -= 10;
     if (cowboyX <= xMin) {
         cowboyX = 0;
     };
     cowboy.style.left = cowboyX + "px";
-    bullet.style.left = cowboyX + "px";
+
 }
 //A DROITE
 function droite() {
-    cowboyX = getComputedStyle(cowboy).left;
-    cowboyX = parseInt(cowboyX) + 10;
+
+    cowboyX += 10;
     if ((cowboyX + diametreCowboy) >= xMaxCowboy) {
         cowboyX = xMaxCowboy - diametreCowboy;
     };
     cowboy.style.left = cowboyX + "px";
-    bullet.style.left = cowboyX + "px";
+
 }
 
-// Collision des bandits avec le cowboy
+
 
 /*if (rect1.x < rect2.x + rect2.width &&
    rect1.x + rect1.width > rect2.x &&
@@ -126,21 +131,15 @@ function droite() {
 }*/
 
 //Debut tir 
-
-
-
-
-
-
 function tir() {
     let myBullet = new Image();
     myBullet.src = 'images/bullet.png';
     myBullet.setAttribute("id", "bullet");
-    cadreEnnemis.appendChild(myBullet);
+    cadreJeu.appendChild(myBullet);
     const bullet = document.getElementById('bullet');
     let topBullet = (parseFloat(getComputedStyle(cowboy).bottom)) + (parseFloat(getComputedStyle(cowboy).height));
     bullet.style.bottom = topBullet + "px";
-
+    bullet.style.left = cowboyX + "px";
     let yBullet = "";
     let newYBullet = "";
     //Si il y a une collision
@@ -164,3 +163,8 @@ function tir() {
     }*/
 }
 //Fin tir
+
+function gameOver() {
+    let btnRejouer = document.createElement('button');
+    cadreJeu.removeChild(bandits);
+}
